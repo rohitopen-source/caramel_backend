f123a0cd72afec75150ccf344b1be9e2d2c32519
rohitopen-source <rohit931999@gmail.com>
2020-05-18 12:50:38 +0530

    updated nodemailer

--- server.js
+++ server.js
@@ -31,12 +31,11 @@ contactRoute.route('/add').post(function(req,res){
     
     contactinfo.save()
                .then(contactinfo => {
+               
                 const nodemailer = require("nodemailer");
 
-                // async..await is not allowed in global scope, must use a wrapper
                 async function main() {
-                  // Generate test SMTP service account from ethereal.email
-                  // Only needed if you don't have a real mail account for testing
+                
                   let testAccount = await nodemailer.createTestAccount();
                 
                   // create reusable transporter object using the default SMTP transport
@@ -60,11 +59,8 @@ contactRoute.route('/add').post(function(req,res){
                   });
                 
                   console.log("Message sent: %s", info.messageId);
-                  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-                
-                  // Preview only available when sending through an Ethereal account
                   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-                  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+                 
                 }
                 
                 main().catch(console.error);
